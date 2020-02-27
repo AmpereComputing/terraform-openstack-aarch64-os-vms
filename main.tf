a19bd0a6338916fabaf61b96364c00855c3366a4
Peter J. Pouliot <peter@pouliot.net>
2020-02-27 07:07:39 +0000

    adding var names to main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,30 @@
 module "centos-7-aarch64-GenericCloud-1708-qcow2" {
-  source       = "./single-vm"
-  image  = "centos-7-aarch64-GenericCloud-1708-qcow2"
+  source  = "./single-vm"
+  image   = "centos-7-aarch64-GenericCloud-1708-qcow2"
+  os_name = "centos7"
 }
 
 module "centos-8-aarch64-GenericCloud-qcow2" {
   source       = "./single-vm"
   image  = "centos-8-aarch64-GenericCloud-qcow2"
-}
-
-module "centos-7_1901-x86_64-qcow2" {
-  source       = "./single-vm"
-  image  = "centos-7.1901-x86_64-qcow2"
+  os_name = "centos8"
 }
 
 module "debian-9-openstack-arm64-qcow2" {
   source       = "./single-vm"
   image  = "debian-9-openstack-arm64-qcow2"
+  os_name = "debian9"
 }
 
 module "debian-10-openstack-arm64-qcow2" {
   source       = "./single-vm"
   image  = "debian-10-openstack-arm64-qcow2"
+  os_name = "debian10"
 }
 
 module "flatcar_current_alpha_arm64_qcow2" {
   source       = "./single-vm"
   image  = "flatcar_current_alpha_arm64_qcow2"
+  os_name = "flatcarLinux"
 }
 
